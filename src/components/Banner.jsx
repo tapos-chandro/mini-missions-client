07dc5400751ac691dd5729c4d11d65198e2223a7
tapos-chandro <taposchandro231@gmail.com>
2025-04-28 18:00:36 +0600

    banner has been done

---
+++ src/components/Banner.jsx
@@ -0,0 +1,78 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Typewriter } from 'react-simple-typewriter'
+import "swiper/css";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+
+const Banner = () => {
+
+    
+    
+    
+
+  const banners = [
+    {
+      id: 1,
+      image: "https://i.ibb.co.com/bjjdMjxL/banner-1.png",
+      title: "Complete Tasks, Earn Rewards",
+      description:
+        " Simplify your workflow by outsourcing small yet essential tasks to a global community of skilled workers. Whether you need quick data entry, content creation, research, or digital marketing support",
+    },
+    {
+      id: 2,
+      image: "https://i.ibb.co.com/HfSn5NF1/banner-2.png",
+      title: "Get Your Tasks Done Fast",
+      description:
+        "Gain full control over the platform with our intuitive and powerful administrative tools. Manage users, oversee task postings, verify submissions, and handle secure payouts with ease. Our system is designed to scale with your growth",
+    },
+    {
+      id: 3,
+      image: "https://i.ibb.co.com/hpGrvv5/banner-3.png",
+      title: "Powerful Management at Your Fingertips",
+      description:
+        "Our platform empowers you to effortlessly manage every aspect of your micro-tasking business — from user registrations and task submissions to secure payouts and performance monitoring.",
+    },
+  ];
+  
+
+
+  return (
+    <Swiper
+      spaceBetween={30}
+      centeredSlides={true}
+      autoplay={{
+        delay: 5500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper " style={{zIndex: -1}}
+    >
+      {banners.map((banner) => (
+        <SwiperSlide key={banner?.id} className="relative mb-5">
+          <img
+            src={banner.image}
+            alt={banner.title}
+            className="w-full object-cover lg:h-[650px] md:h-[500px] h-[300px] "
+          />
+          <div className=" absolute top-0 h-full flex justify-center flex-col items-center w-full p-5 bg-[#000000a5] ">
+            <div className="max-w-4xl text-center space-y-2 ">
+              <h2 className="lg:text-6xl font-bold text-xl text-primary-text  ">
+               
+                <Typewriter words={[banner.title]} loop={true} cursor  />
+              </h2>
+              <p className="text-secondary-text lg:text-xl text-sm ">
+                {banner.description}
+              </p>
+              <button className="btn bg-primary-color border-none text-primary-text">See More</button>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,22 +5,21 @@ import { Link, NavLink } from 'react-router-dom';
 import logo from "../../src/assets/images/logo-0.png"
 
 const Navbar = () => {
-
-
     const navLinks = <>
         <NavLink className="px-2 bg-transparent border-none hover:border lg:text-md  btn m-2 text-secondary-text" to={'/'}>Home</NavLink>
         <NavLink className="px-2 bg-transparent border-none hover:border lg:text-md  btn m-2 text-secondary-text" to={'/dashboard'}>Dashboard</NavLink>
         <NavLink className="px-2 bg-transparent border-none hover:border lg:text-md  btn m-2 text-secondary-text" to={'/coin'}>Available Coin </NavLink>
-
         <NavLink className="px-2 bg-transparent border-none hover:border lg:text-md  btn m-2 text-secondary-text" to={'/login'}>Login</NavLink>
         <NavLink className="px-2 bg-transparent border-none hover:border lg:text-md  btn m-2 text-secondary-text" to={'/register'}>Register</NavLink>
         <NavLink className="px-2 bg-transparent border-none hover:border lg:text-md  btn m-2 text-secondary-text" to={'https://github.com/Programming-Hero-Web-Course4/b10a12-client-side-tapos-chandro.git'} target="_blank">Join as Developer</NavLink>
     </>
 
+
+
     return (
         <div className="navbar">
             <div className="navbar-start">
-                <Link to={'/'} className="text-xl"><img src={logo} className='max-w-60' alt="" srcset="" /></Link>
+                <Link to={'/'} className="text-xl"><img src={logo} className='lg:w-60 w-52 ' alt="" srcset="" /></Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
@@ -32,7 +31,7 @@ const Navbar = () => {
                     <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
                     <div className="drawer-content">
                         {/* Page content here */}
-                        <label htmlFor="my-drawer-4" className="drawer-button btn bg-primary-color "><IoMenu  className='text-2xl text-primary-text '/></label>
+                        <label htmlFor="my-drawer-4" className="drawer-button btn bg-primary-color "><IoMenu  className='text-xl text-primary-text '/></label>
                     </div>
                     <div className="drawer-side">
                         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
